8cc99f723c8917c1fd33ae2c832f87c299d35692
Lokesh <lokeshbargujar15@gmail.com>
2026-09-11 19:28:20 +0530

    this is the second push

--- test.py
+++ test.py
@@ -3,3 +3,21 @@ age = int(input("Enter your age: "))
 
 print("Hello", name)
 print("You are", age, "years old")
+
+# Calculate age next year
+next_age = age + 1
+print("Next year, you will be", next_age)
+
+# Check whether the person is an adult
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are under 18.")
+
+# Greeting based on age
+if age < 13:
+    print("You are a child.")
+elif age < 20:
+    print("You are a teenager.")
+else:
+    print("You are an adult.")
